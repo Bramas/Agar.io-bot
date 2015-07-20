a1216a138e48baa858c09c076ed25f374f89cefe
Bramas <quentin.bramas@gmail.com>
2015-07-20 17:10:22 +0200

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -755,7 +755,7 @@ console.log("Running Apos Bot!");
                         
                         var splitDangerDistance = allPossibleThreats[i].size + splitDistance + 10 + shiftDistance;
 
-                        var normalDangerDistance = allPossibleThreats[i].size + 40 + shiftDistance;
+                        var normalDangerDistance = allPossibleThreats[i].size + 15 + shiftDistance;
 
 
                         //console.log("Found distance.");
